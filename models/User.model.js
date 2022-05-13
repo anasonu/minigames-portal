190ac9a4e6080101e3009af4744e3796724c839d
Ana Sobrino <anasonu@hotmail.com>
2022-05-13 11:01:20 +0200

    Definido el UserModel

--- models/User.model.js
+++ models/User.model.js
@@ -3,11 +3,24 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
   {
+    email: {
+      type: String,
+      unique: true,
+      required: true
+    },
     username: {
       type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
+      required: true,
+      unique: true,
     },
-    password: String,
+    password: {
+      type: String,
+      required: true,
+    },
+    admin: {
+      type: Boolean,
+      default: false,
+    }
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
@@ -15,6 +28,6 @@ const userSchema = new Schema(
   }
 );
 
-const User = model("User", userSchema);
+const UserModel = model("user", userSchema);
 
-module.exports = User;
+module.exports = UserModel;
