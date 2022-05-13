867b39f6b3f49030b82f446f11bbc55ca5659a1b
Andrea Sanchez <asv1994@hotmail.com>
2022-05-13 12:55:14 +0200

    avanzando parte juegos

--- routes/games.routes.js
+++ routes/games.routes.js
@@ -1,8 +1,63 @@
 const router = require("express").Router();
 
+const GameModel = require("../models/Game.model.js");
+
+// GET "/games" => lista de todos los juegos
+router.get("/", (req,res,next) => {
+
+    GameModel.find()
+    .then((allGames) => {
+        res.render("games/list.hbs", {
+            listGames: allGames
+        })
+    })
+    .catch((err) => {
+        next(err);
+    })
+})
+
+// GET "/games/create" => renderiza formulario de añadir un juego
+router.get("/create", (req, res, next) => {
+    res.render("games/add.hbs")
+})
+
+// POST "/games/create" => añade un objeto a la coleccion de juegos de la bd, y redirecciona al listado
+router.post("/create", async (req,res,next) => {
+    const { titulo, creador, descripcion, url } = req.body;
+
+    try {
+        const game = await GameModel.create( {
+            titulo,
+            creador,
+            descripcion,
+            url
+        })
+        res.redirect(`/games/${game._id}/details`);
+
+    }
+    catch (err) {
+        next(err);
+    }
+})
+
+
+// GET "/games/:id/details" => muestra los detajes del juego seleccionado
+router.get("/:id/details", (req,res,next) => {
+    const { id } = req.params;
+
+    GameModel.findById(id)
+    .then((game) => {
+        res.render("games/details.hbs", {
+            gameDetails: game
+        })
+    })
+    .catch((err) => {
+        next(err);
+    })
 
 
 
+})
 
 
 
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 
 /* GET home page */
-router.get("/", (req, res, next) => {
-  res.render("games/list.hbs");
-});
+// router.get("/", (req, res, next) => {
+  
+//   res.render("games/list.hbs");
+// });
 
 const gameRoutes = require("./games.routes.js")
 router.use("/games", gameRoutes)
