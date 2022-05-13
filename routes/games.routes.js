15b7f0ba970294402355bd443958fda7fa5e63f6
Andrea Sanchez <asv1994@hotmail.com>
2022-05-13 16:49:02 +0200

    avanzando

--- routes/games.routes.js
+++ routes/games.routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 
+const { render } = require("express/lib/response");
 const GameModel = require("../models/Game.model.js");
 
 // GET "/games/create" => renderiza formulario de añadir un juego
@@ -10,7 +11,7 @@ router.get("/create", (req, res, next) => {
 // POST "/games/create" => añade un objeto a la coleccion de juegos de la bd, y redirecciona al listado
 router.post("/create", async (req,res,next) => {
     const { titulo, creador, descripcion, url } = req.body;
-    
+
     try {
         const game = await GameModel.create( {
             titulo,
@@ -27,7 +28,7 @@ router.post("/create", async (req,res,next) => {
 })
 
 
-// GET "/games/:id/details" => muestra los detajes del juego seleccionado
+// GET "/games/:id/details" => muestra los detalles del juego seleccionado
 router.get("/:id/details", (req,res,next) => {
     const { id } = req.params;
 
@@ -40,11 +41,42 @@ router.get("/:id/details", (req,res,next) => {
     .catch((err) => {
         next(err);
     })
+})
 
+// GET "/games/:id/edit" => pagina de edicion del juego seleccionado
+router.get("/:id/edit", (req, res, next) => {
+    const { id } = req.params
 
+    GameModel.findById(id)
+    .then((game) => {
+        res.render(`games/edit.hbs`, {
+            gameEdit: game
+        })
+    })
+    .catch((err) => {
+        next(err)
+    })
 
 })
 
+
+// POST "/games/:id/edit" => edita los datos del juego seleccionado
+// router.get("/:id/edit", (req, res, next) => {
+//     const { id } = req.params
+
+//     const { titulo, creador, descripcion, url } = req.body;
+
+//     GameModel.findByIdAndUpdate(id, {
+//         titulo, 
+//         creador, 
+//         descripcion, 
+//         url
+//     })
+//     .then((gameUpdate) => {
+//         res.render(`games/details.hbs`)
+//     })
+    
+// })
 
 
 module.exports = router;
